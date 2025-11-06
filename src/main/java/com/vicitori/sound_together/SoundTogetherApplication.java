b091585a3b5b2e351731840a3b05ef70451471a0
Victoria Ostrovskaya <victoria.v.ostrovskaya@gmail.com>
2025-11-06 12:02:11 +0300

    chore: init

---
+++ src/main/java/com/vicitori/sound_together/SoundTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.vicitori.sound_together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundTogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vicitori/sound_together/SoundTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicitori.sound_together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
