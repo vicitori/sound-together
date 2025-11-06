c68731dfba05422a7b5f1e7166c0ded703a875a7
Victoria Ostrovskaya <victoria.v.ostrovskaya@gmail.com>
2025-11-06 13:45:29 +0300

    chore: dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,8 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
