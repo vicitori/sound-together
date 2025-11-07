817a73cb9262341de1af1c07d75893c494e6326f
Victoria Ostrovskaya <victoria.v.ostrovskaya@gmail.com>
2025-11-07 16:46:00 +0300

    fix: service annotation, rate checking

--- src/main/java/com/vicitori/sound_together/service/VoteService.java
+++ src/main/java/com/vicitori/sound_together/service/VoteService.java
@@ -4,15 +4,20 @@ import com.vicitori.sound_together.model.AppUser;
 import com.vicitori.sound_together.model.Track;
 import com.vicitori.sound_together.model.Vote;
 import com.vicitori.sound_together.repository.VoteRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class VoteService {
     private TrackService trackService;
     private AppUserService userService;
     private VoteRepository repository;
 
     public Vote vote(int rate, Long trackId, Long userId) {
+        if (rate < 0 || rate > 10) {
+            throw new IllegalArgumentException("rate should be from 0 to 10");
+        }
         Track track = trackService.getTrackById(trackId).orElseThrow(() -> new RuntimeException("can not find track with id " + trackId + ". try input other trackId."));
         AppUser user = userService.getUserById(userId).orElseThrow(() -> new RuntimeException("can not find user with id " + userId + ". try input other userId."));
         Vote vote = new Vote(rate, track, user);
