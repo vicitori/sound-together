3ae0d10362805acfadbfb1ccf9e8f6e9f10c2f2a
Victoria Ostrovskaya <victoria.v.ostrovskaya@gmail.com>
2025-11-11 12:32:50 +0300

    ref: repositories according to services

--- src/main/java/com/vicitori/sound_together/repository/PlaylistRepository.java
+++ src/main/java/com/vicitori/sound_together/repository/PlaylistRepository.java
@@ -3,11 +3,14 @@ package com.vicitori.sound_together.repository;
 import com.vicitori.sound_together.model.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 @Repository
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
     Optional<Playlist> findByPlistName(String plistName);
+
     Optional<Playlist> findByPlistShareCode(String plistShareCode);
+
     boolean existsByPlistShareCode(String plistShareCode);
 }
--- src/main/java/com/vicitori/sound_together/repository/VoteRepository.java
+++ src/main/java/com/vicitori/sound_together/repository/VoteRepository.java
@@ -1,9 +1,12 @@
 package com.vicitori.sound_together.repository;
 
+import com.vicitori.sound_together.model.AppUser;
+import com.vicitori.sound_together.model.Track;
 import com.vicitori.sound_together.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+    boolean existsByTrackAndAppUser(Track track, AppUser appUser);
 }
